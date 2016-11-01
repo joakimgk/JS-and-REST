e7d70caaf435b4ff9529b942ee13e2c23af317ca
Joakim Knudsen <joakim.knudsen@webstep.no>
2016-11-01 19:15:48 +0100

    Added Morgan, for logging

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');  // import Express (JS) module
+const morgan = require('morgan');
 
 const app = express();
+app.use(morgan('dev'));
 
 app.get('/', function(request, response) {
+  console.log("Noen aksesserer /");
   response.send('Hello World');
 });
 
 const port = 3000;
 app.listen(port, function() {
+  //console.log('TEST: port ' + port);
   console.log(`Example app listening on port ${port}`);
 });
